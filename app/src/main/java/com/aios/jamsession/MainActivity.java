caf13b68bf6f6396687c2908200e65ba57a0777f
Aranza <aranzaiortegas@gmail.com>
2021-05-02 13:44:53 +0200

    activity login and register created with views

--- app/src/main/java/com/aios/jamsession/MainActivity.java
+++ app/src/main/java/com/aios/jamsession/MainActivity.java
@@ -4,18 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView mTextViewRegister;
+import com.google.android.material.textfield.TextInputEditText;
 
+public class MainActivity extends AppCompatActivity {
+
+    // Variables
+    TextView mTextViewRegister;
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mLoginButton;
+
+    //Methods
+
+    // Activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Find and assign the variables that are in the xml script
         mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mLoginButton = findViewById(R.id.loginButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        };
+
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,5 +47,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    // To login in app
+    private void login(){
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+        Log.d("CAMPO", "email: " + email);
+        Log.d("CAMPO", "password: " + password);
     }
 }
--- app/src/main/java/com/aios/jamsession/RegisterActivity.java
+++ app/src/main/java/com/aios/jamsession/RegisterActivity.java
@@ -2,13 +2,31 @@ package com.aios.jamsession;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    // Variables
+    TextView mTextViewLogin;
+
+    //Functions
+
+    // Activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mTextViewLogin = findViewById(R.id.textViewLogin);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
